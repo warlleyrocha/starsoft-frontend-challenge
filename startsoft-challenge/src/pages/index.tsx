68a34614f21c9bc25ba1e983ff29d0263f281291
warlleyrocha <warlleyrocha@icloud.com>
2026-02-14 21:23:41 -0300

    feat(footer): implement footer component and integrate into home page

--- startsoft-challenge/src/pages/index.tsx
+++ startsoft-challenge/src/pages/index.tsx
@@ -1,11 +1,12 @@
 import { Header } from "@/shared/components/Header";
+import { Footer } from "@/shared/components/Footer";
 
 export default function Home() {
   return (
     <>
-      <Header cartCount={3} />
-
+      <Header cartCount={2} />
       <main></main>
+      <Footer />
     </>
   );
 }
---
+++ startsoft-challenge/src/shared/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import styles from "./Footer.module.scss";
+
+export function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p className={styles.text}>STARSOFT © TODOS OS DIREITOS RESERVADOS</p>
+    </footer>
+  );
+}
